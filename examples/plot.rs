6dd01f422e7a110ccaee979154f2b591757d2a24
KOKI <nekomehako@gmail.com>
2022-09-09 23:59:10 +0900

    Fixed a small problem

--- examples/plot.rs
+++ examples/plot.rs
@@ -1,8 +1,7 @@
-
-fn main(){
+fn main() {
     let range = 0..100;
     let sin_iter = (0..150).map(|x| (x as f32 / 10.0).sin() * 20.0);
-    let parabola_vec:Vec<i32> = (-10..10).map(|x| x*x - 30).collect();
+    let parabola_vec: Vec<i32> = (-10..10).map(|x| x * x - 30).collect();
     let cos_vec: Vec<f32> = (0..150).map(|x| (x as f32 / 20.0).cos() * 10.0).collect();
     let cos_slcie: &[f32] = &cos_vec;
 
